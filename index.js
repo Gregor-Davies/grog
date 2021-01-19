575f68a00cef2ea3f58c8eb52effecb3f2801cd2
Gregor Davies <43633955+Gregor-Davies@users.noreply.github.com>
2021-01-19 09:05:06 +0000

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-const { token, prefix } = require('./config.json');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-	console.log('Logged in as Grog!');
-});
-
-client.login(token);
